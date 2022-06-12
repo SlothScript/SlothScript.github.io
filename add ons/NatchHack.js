8a0295baaeaf8a89c681faaefc81dbf0fc085a34
SlothScript <106759481+SlothScript@users.noreply.github.com>
2022-06-12 09:15:51 -0500

    Create NatchHack.js
    Yeah this simple mod allows you to cheat in Cookies, Heavenly Chips, and sugar lumps.

---
+++ add ons/NatchHack.js
@@ -0,0 +1,6 @@
+var cookies = parseInt(prompt("How many cookies?"))
+Game.cookies = cookies
+var HC = parseInt(prompt("How many HC's (Heavenly Chips)?"))
+Game.heavenlyChips = HC
+var SugarLumps = parseInt(prompt("How many sugar lumps??"))
+Game.lumps = SugarLumps
